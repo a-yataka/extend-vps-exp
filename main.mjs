77065207b7de21593fded9f11c4b6df5e69110bf
a-yataka <japan.zaas5572@gmail.com>
2025-07-27 03:04:25 +0900

    wip

--- main.mjs
+++ main.mjs
@@ -40,6 +40,7 @@ try {
     await page.locator('[placeholder="上の画像の数字を入力"]').fill(code)
     await page.waitForNavigation({ waitUntil: 'networkidle2' })
 
+    await page.waitForTimeout(5000);
     // await page.locator(".cb-i").click()
     await page.click('.cb-lb input[type="checkbox"]');
 
